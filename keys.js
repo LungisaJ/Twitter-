b9662772c2807a6358d6a6359643abfbe28adb4f
Lungisa <lungisajohnson@gmail.com>
2017-04-13 22:23:53 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'z7hvaqM656jFN3jBCxo9WzHTp',
+  consumer_secret:      'leYoQoDvkWih9k4CV3RwymFzMpA7Zk4gKjBfuJ0tC8lTQQQFbx',
+  access_token:         '850871785616793602-kpG6rF00P6QHumr6mj929nRQJ2lj15a',
+  access_token_secret:  'LGBotVeEFEGJlcKtd3pYrt1rWJA6J8Gf0xQVVlzq8N1vf',
+  };
---
+++ liri.js
@@ -0,0 +1,35 @@
+var Twit = require("twit");
+
+var keys = require('./keys');
+var T = new Twit(keys);
+
+/*var params =  {
+  q: 'coding',
+  count: 2
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+var tweets = data.statuses;
+for (var i = 0; i < tweets.length, i++) {
+  console.log(tweets[i].text);
+}*/
+
+
+//  console.log(data);
+//}
+//posting function
+var tweet = {
+  status: '#coding homework from node.js'
+}
+
+T.post('statuses/update', { status: 'Hello Twitter World!' },
+  function(err, data, response) {
+    console.log(data)
+  })
+
+T.get('search/tweets', { q: 'rainbow', count: 5 },
+  function(err, data, response) {
+    console.log(data)
+})
